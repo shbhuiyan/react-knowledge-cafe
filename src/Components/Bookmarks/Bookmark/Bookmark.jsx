f953f6040f3ee84cb2628e6e4675e7fc3622d4ff
shbhuiyan <shbhuiyan997@gmail.com>
2024-10-24 17:45:24 +0600

    added bookmarks button

--- src/Components/Bookmarks/Bookmark/Bookmark.jsx
+++ src/Components/Bookmarks/Bookmark/Bookmark.jsx
@@ -1,9 +1,16 @@
-const Bookmark = () => {
+import PropTypes from "prop-types";
+
+const Bookmark = ({bookmark}) => {
+    
+    const {title} = bookmark;
+
     return (
-        <div>
-            
-        </div>
+        <div className="p-4 my-4 bg-white rounded-lg"> {title} </div>
     );
 };
+
+Bookmark.propTypes = {
+    bookmark: PropTypes.object.isRequired,
+}
 
 export default Bookmark;
--- src/Components/Bookmarks/Bookmarks.jsx
+++ src/Components/Bookmarks/Bookmarks.jsx
@@ -1,20 +1,26 @@
 import PropTypes from "prop-types";
+import Bookmark from "./Bookmark/Bookmark";
 
-const Bookmarks = ({bookmarks}) => {
-    return (
-        <section className="w-1/3 ml-4 space-y-6">
-            <h3 className="text-center font-semibold text-2xl p-4 border-2 bg-slate-200 border-purple-500 rounded-lg text-blue-500">Total Reading Time :  min</h3>
+const Bookmarks = ({ bookmarks }) => {
 
-            <div className='text-center font-semibold text-2xl p-4 border-2 bg-slate-200 rounded-lg'>
-                <h3>Bookmarked Blogs : {bookmarks.length} </h3>
-            </div>
-        </section>
-    );
+  return (
+    <section className="w-1/3 ml-4 space-y-6">
+      <h3 className="text-center font-semibold text-2xl p-4 border-2 bg-slate-200 border-purple-500 rounded-lg text-blue-500">
+        Total Reading Time : min
+      </h3>
+
+      <div className="text-center font-semibold text-2xl p-4 border-2 bg-slate-200 rounded-lg">
+        <h3>Bookmarked Blogs : {bookmarks.length} </h3>
+          {
+            bookmarks.map((bookmark , i)=> <Bookmark key={i} bookmark={bookmark}></Bookmark>)
+          }
+      </div>
+    </section>
+  );
 };
 
-
 Bookmarks.propTypes = {
-    bookmarks: PropTypes.array.isRequired,
-}
+  bookmarks: PropTypes.array.isRequired,
+};
 
 export default Bookmarks;
